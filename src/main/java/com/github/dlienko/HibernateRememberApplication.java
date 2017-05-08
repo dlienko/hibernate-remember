7d79b464f393493f7087ff38d78cebe1571ca08f
Dmytro Lienko <dlienko@egnyte.com>
2017-05-08 16:29:09 +0200

    add Spring Boot project

---
+++ src/main/java/com/github/dlienko/HibernateRememberApplication.java
@@ -0,0 +1,12 @@
+package com.github.dlienko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateRememberApplication.class, args);
+	}
+}
