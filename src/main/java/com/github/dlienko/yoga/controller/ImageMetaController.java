912b31cd4d607bf4858b2789ba07f1f2819f9baf
Dmytro Lienko <dlienko@egnyte.com>
2017-07-16 10:21:43 +0200

    add image deleting method

--- src/main/java/com/github/dlienko/yoga/controller/ImageMetaController.java
+++ src/main/java/com/github/dlienko/yoga/controller/ImageMetaController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,6 +48,11 @@ public class ImageMetaController {
     public ImageMeta getById(@PathVariable("id")UUID id) {
         ImageEntity image = imageRepository.findOne(id);
         return conversionService.toImageMeta(image);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable("id") UUID id) {
+        imageRepository.delete(id);
     }
 
 }
